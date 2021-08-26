f159cf5381b9c37c9720efc03a8c60bcb5092967
nishantp <nishantp-iprogrammer.co>
2021-08-26 18:31:57 +0530

    v1

--- src/app/pages/datalist/datalist.component.ts
+++ src/app/pages/datalist/datalist.component.ts
@@ -10,39 +10,43 @@ import { timer } from 'rxjs';
   templateUrl: './datalist.component.html',
   styleUrls: ['./datalist.component.scss']
 })
-export class DatalistComponent implements OnInit, AfterViewInit {
+export class DatalistComponent implements OnInit {
 
   @ViewChild('scroller') scroller: CdkVirtualScrollViewport | any;
   comp_data_list: ResponseModel[] = [];
   loading: boolean = false;
 
   constructor(
+    // ----------- Uncomment for resolver based view---------------
     // private route: ActivatedRoute,
     private ngZone: NgZone,
     private dataSrv: DataService
   ) { }
 
   ngOnInit(): void {
+    // ------------------ Uncomment below code for resolver based view---------------
     // this.loading = true;
     // this.comp_data_list = [];
     // this.comp_data_list = this.route.snapshot.data.resolveData;
     // this.loading = false;
-    this.getData();
+    // ---------------------------------------------------------------------------
+    this.getData(); // Comment out for resolver based view
   }
 
-  ngAfterViewInit(): void {
-    this.scroller.elementScrolled().pipe(
-      map(() => this.scroller.measureScrollOffset('bottom')),
-      pairwise(),
-      filter(([y1, y2]) => (y2 < y1 && y2 < 140)),
-      throttleTime(200)
-    ).subscribe(() => {
-      this.ngZone.run(() => {
-        this.getData();
-      });
-    }
-    );
-  }
+  // -----------------Uncomment for resolver based view------------------
+  // ngAfterViewInit(): void {
+  //   this.scroller.elementScrolled().pipe(
+  //     map(() => this.scroller.measureScrollOffset('bottom')),
+  //     pairwise(),
+  //     filter(([y1, y2]) => (y2 < y1 && y2 < 140)),
+  //     throttleTime(200)
+  //   ).subscribe(() => {
+  //     this.ngZone.run(() => {
+  //       this.getData();
+  //     });
+  //   }
+  //   );
+  // }
 
   getData() {
     this.dataSrv.getDataList().subscribe(res => {
