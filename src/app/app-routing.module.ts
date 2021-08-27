de1a7359d5ee3f3ea6941f8347d7fe458585e2f2
nishantp <nishantp-iprogrammer.co>
2021-08-27 11:53:31 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'cart',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
   },
 ];
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(
+    private fBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.fBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    if (this.loginForm.controls.email.value === 'demo@mail.com' && this.loginForm.controls.password.value === '123456') {
+      this.router.navigateByUrl('/cart');
+    }
+  }
+
+}
